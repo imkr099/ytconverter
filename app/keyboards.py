f3713817c74ecd479d9716511de7582621904180
Islambek M <imkr099@gmail.com>
2024-11-24 12:52:57 +0600

    f2

--- app/keyboards.py
+++ app/keyboards.py
@@ -5,7 +5,7 @@ async def choice_button():
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='mp4(Video🎥)', callback_data='mp4'),
          InlineKeyboardButton(text='mp3(Audio🎧)', callback_data='mp3')],
-        [InlineKeyboardButton(text='Cancel❌', callback_data='cancel')]
+        [InlineKeyboardButton(text='Отмена❌', callback_data='cancel')]
     ])
     return keyboard
 
@@ -14,5 +14,5 @@ async def create_quality_buttons(qualities):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[])
     for quality in qualities:
         keyboard.inline_keyboard.append([InlineKeyboardButton(text=quality, callback_data=quality)])
-    keyboard.inline_keyboard.append([InlineKeyboardButton(text='◀️Back', callback_data='back')])
+    keyboard.inline_keyboard.append([InlineKeyboardButton(text='◀️Назад', callback_data='back')])
     return keyboard
